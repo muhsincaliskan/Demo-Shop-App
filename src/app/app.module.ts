75f8dccc0bb3836f5f5d6655fe791b7728394ee1
killerbean26 <34723829+killerbean26@users.noreply.github.com>
2020-08-01 12:50:51 +0300

    demo shop app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{FormsModule} from '@angular/forms';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +10,10 @@ import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
 import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
+import { LoginComponent } from './login/login.component';
+import { AccountService } from './services/account.service';
+import { LoginGuard } from './login/login.guard';
 
 @NgModule({
   declarations: [
@@ -18,16 +22,19 @@ import { ProductAddForms1Component } from './product/product-add-forms1/product-
     CategoryComponent,
     ProductComponent,
     ProductFilterPipe,
-    ProductAddForms1Component
+    ProductAddForms1Component,
+    ProductAddForms2Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   //service declaration
-  providers: [AlertifyService],
+  providers: [AlertifyService,AccountService,LoginGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
 import { Product } from '../product/product'
 import { Observable, throwError } from 'rxjs'
-import {tap,catchError} from 'rxjs/operators'
+import { tap, catchError } from 'rxjs/operators'
 @Injectable()
 export class ProductService {
 
@@ -10,25 +10,37 @@ export class ProductService {
   path = "http://localhost:3000/products"
   //Observable implementation
   getProducts(categoryId): Observable<Product[]> {
-    let newPath=this.path
+    let newPath = this.path
     if (categoryId) {
-      newPath+="?categoryId="+categoryId
+      newPath += "?categoryId=" + categoryId
     }
-    
+
     return this.http.get<Product[]>(newPath).pipe(
-      tap(data=>console.log(JSON.stringify(data))),
+      tap(data => console.log(JSON.stringify(data))),
       catchError(this.handleError)
     )
 
   }
-  handleError(err:HttpErrorResponse){
-    let errorMsg=""
-    if (err.error instanceof ErrorEvent) {
-      errorMsg="Error"+err.error.message
+  addProduct(product: Product): Observable<Product> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      })
     }
-    else{
-      errorMsg="system error"
-    }    
+    return this.http.post<Product>(this.path, product,).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    )
+  }
+  handleError(err: HttpErrorResponse) {
+    let errorMsg = ""
+    if (err.error instanceof ErrorEvent) {
+      errorMsg = "Error" + err.error.message
+    }
+    else {
+      errorMsg = "system error"
+    }
     return throwError(errorMsg)
   }
 }
